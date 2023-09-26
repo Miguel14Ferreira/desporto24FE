4328055f877c38b1fb93471de5138a0fc026a69e
Miguel Ferreira <tmfftw@gmail.com>
2023-09-26 15:09:02 +0100

    novasAlteracoes

--- src/app/enum/header-type.enum.ts
+++ src/app/enum/header-type.enum.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export enum HeaderType{
     AUTHORIZATION = 'Authorization',
     JWT_TOKEN = 'JWT-Token',
     JWT_REFRESH_TOKEN = 'JWT-Refresh-Token',
-=======
-export enum HeaderType{
-    AUTHORIZATION = 'Authorization',
-    JWT_TOKEN = 'JWT-Token',
-    JWT_REFRESH_TOKEN = 'JWT-Refresh-Token',
->>>>>>> 202330b3f2eb933769daa09fff17e79d050c114f
 }
